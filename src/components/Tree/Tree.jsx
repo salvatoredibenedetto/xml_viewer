d0b70143ed49a8d90d61e1c674d0a6173ef6e671
Salvatore Dibenedetto <dibenedetto.me@gmail.com>
2022-05-21 10:38:18 +0200

    get xml root

--- src/components/Tree/Tree.jsx
+++ src/components/Tree/Tree.jsx
@@ -6,9 +6,9 @@ const Tree = () => {
   const { rawXML } = useContext(Context);
 
   if (rawXML) {
-    const xmlToJson = convert.xml2json(rawXML, { compact: true });
-    const data = JSON.parse(xmlToJson);
-    console.log(data);
+    const xmlToJson = convert.xml2json(rawXML, { ignoreDeclaration: true });
+    const [xmlRoot] = JSON.parse(xmlToJson).elements;
+    console.log(xmlRoot);
     debugger;
   }
   return <div>{(rawXML && "converting data...") || "No data to show"}</div>;
